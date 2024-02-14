523007ac1b4ce99cd6a0ee65217fc5ba418b7006
AhmedKyali <ahmed.kaiialy@gmail.com>
2024-02-14 13:18:54 +0200

    V0.2

--- Numeric_project.py
+++ Numeric_project.py
@@ -10,10 +10,16 @@ import numpy as np
 
 def iter_com():
     req_iter_entry.configure(state='normal')
+    button1.configure(hover_color='#ededed', fg_color='#5c5b5b')
+    button2.configure(hover_color='#ffffff', fg_color='#ffffff')
+    solve_button.configure(text='Solve : By Iterations')
     req_Error_entry.delete('0', END)
     req_Error_entry.configure(state='disabled')
 def error_com():
     req_Error_entry.configure(state='normal')
+    button1.configure(hover_color='#ffffff', fg_color='#ffffff')
+    button2.configure(hover_color='#ededed', fg_color='#5c5b5b')
+    solve_button.configure(text='Solve : By Error')
     req_iter_entry.delete('0', END)
     req_iter_entry.configure(state='disabled')
 def solve_equation():
@@ -93,19 +99,19 @@ frame2 = CTkFrame(root)
 frame2.pack(fill='x')
 CTkLabel(frame2, text="Lower Value (x_l):", anchor='w').pack(side='left')
 x_l_entry = CTkEntry(frame2, placeholder_text="EX: 0...")
-x_l_entry.pack(side='right', ipadx=22)
+x_l_entry.pack(side='right', ipadx=22, pady=5)
 
 frame3 = CTkFrame(root)
 frame3.pack(fill='x')
 CTkLabel(frame3, text="Upper Value (x_u):", anchor='w').pack(side='left')
 x_u_entry = CTkEntry(frame3, placeholder_text="EX: 1...")
-x_u_entry.pack(side='right', ipadx=22)
+x_u_entry.pack(side='right', ipadx=22, pady=5)
 
 frame4 = CTkFrame(root)
 frame4.pack(fill='x')
 CTkLabel(frame4, text="F(x):", anchor='w').pack(side='left')
 fx_entry = CTkEntry(frame4, placeholder_text="EX: 2x^3-4x+5...")
-fx_entry.pack(side='right', ipadx=22)
+fx_entry.pack(side='right', ipadx=22, pady=5)
 
 # Create a new frame for the last two label-entry pairs and the buttons
 frame_last = CTkFrame(root)
@@ -115,22 +121,22 @@ frame1 = CTkFrame(frame_last)
 frame1.pack(side='left', fill='x', expand=True)
 CTkLabel(frame1, text="Requested Error Percentage:", anchor='w').pack(side='left')
 req_Error_entry = CTkEntry(frame1, placeholder_text="EX: 10...", state='disabled')
-req_Error_entry.pack(side='right', ipadx=22)
+req_Error_entry.pack(side='right', ipadx=22, pady=5)
 
 image_file = ImageTk.PhotoImage(Image.open("img.png").resize((20, 20), Image.Resampling.LANCZOS))
 image_file2 = ImageTk.PhotoImage(Image.open("img2.png").resize((20, 20), Image.Resampling.LANCZOS))
 # Create the first button and pack it to the left in frame_last
 button1 = CTkButton(frame_last, image=image_file2, text="", command=error_com, corner_radius=35, border_color='#ffffff',
                     border_width=2, width=20, hover_color='#ededed', fg_color='#5c5b5b')
-button1.pack(side='left', ipadx=20)
+button1.pack(side='left', ipadx=20, padx=5)
 
 # Create the second button and pack it to the left in frame_last
 button2 = CTkButton(frame_last, image=image_file, text="", corner_radius=35, command=iter_com, border_color='#ffffff',
                     border_width=2, width=20, hover_color='#ededed', fg_color='#5c5b5b')
-button2.pack(side='left', ipadx=20)
+button2.pack(side='left', ipadx=20, padx=5)
 
 frame5 = CTkFrame(frame_last)
-frame5.pack(side='left', fill='x',ipadx=25, expand=True)
+frame5.pack(side='left', fill='x', ipadx=25, expand=True)
 CTkLabel(frame5, text="Requested iterations:", anchor='e').pack(side='left')
 req_iter_entry = CTkEntry(frame5, placeholder_text="EX: 5...", state='disabled')
 req_iter_entry.pack(side='right', ipadx=22)
@@ -139,17 +145,18 @@ req_iter_entry.pack(side='right', ipadx=22)
 # Button to solve equation
 solve_button = CTkButton(master=root, text="Solve", command=solve_equation, corner_radius=35, fg_color="transparent",
                          hover_color="#5c5b5b", border_color='#ffffff', border_width=2)
-solve_button.pack()
+solve_button.pack(pady=10)
 
 
 
 # Output Text widget
 frame_t_f = CTkScrollableFrame(root)
+frame_t_f.pack(fill='both', expand=True)
 tabview = CTkTabview(master=frame_t_f)
 tabview.pack(fill='both', expand=True)
 tabview.add('Table')
 tabview.add('Chart')
-frame_t_f.pack(fill='both', expand=True)
+
 table = ttk.Treeview(tabview.tab('Table'), columns=('i', 'x_l', 'F(x_l)', 'x_u', 'F(x_u)', 'x_r', 'F(x_r)', 'ε_a'),
                      show='headings', selectmode="extended")
 table.heading('i', text='i')

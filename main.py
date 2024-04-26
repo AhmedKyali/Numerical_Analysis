25c712aa317ad5ecf81d1534f819d351e7c3490c
AhmedKyali <ahmed.kaiialy@gmail.com>
2024-04-26 06:32:35 +0300

    V0.81

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bracketing import bracket
 from Newton import newton_method
 from Secant import Secant_method
 from Simple_fixed import Simple_fixed_method
+from Gaussian_Elimination import Gauss_e
+from Gaussian_Jordan_Elimination import Gauss_j_e
+
 
 menu = CTk()
 menu.title("Main Menu")
@@ -18,18 +21,26 @@ frame2.pack(fill='both', expand=True)
 
 button1 = CTkButton(frame2, text="Bracketing Methods", corner_radius=35, command=bracket, border_color='#ffffff',
                     border_width=2, hover_color='#ededed', fg_color='#5c5b5b')
-button1.pack(side='left', padx=10)
+button1.pack(side='top', fill='x')
 
 button2 = CTkButton(frame2, text="Newton Method", corner_radius=35, border_color='#ffffff',
                     border_width=2, hover_color='#ededed', fg_color='#5c5b5b', command=newton_method)
-button2.pack(side='right', padx=10)
+button2.pack(side='top', fill='x')
 
 button3 = CTkButton(frame2, text="Secant Method", corner_radius=35, border_color='#ffffff',
                     border_width=2, hover_color='#ededed', fg_color='#5c5b5b', command=Secant_method)
-button3.pack(side='right', padx=10)
+button3.pack(side='top', fill='x')
 
 button4 = CTkButton(frame2, text="Simple Fixed point", corner_radius=35, border_color='#ffffff',
                     border_width=2, hover_color='#ededed', fg_color='#5c5b5b', command=Simple_fixed_method)
-button4.pack(side='left', pady=10)
+button4.pack(side='top', fill='x')
+
+button5 = CTkButton(frame2, text="Gaussian Elimination(Partial Pivoting)", corner_radius=35, border_color='#ffffff',
+                    border_width=2, hover_color='#ededed', fg_color='#5c5b5b', command=Gauss_e)
+button5.pack(side='top', fill='x')
+
+button5 = CTkButton(frame2, text="Gaussian Jordan Elimination(Partial Pivoting)", corner_radius=35, border_color='#ffffff',
+                    border_width=2, hover_color='#ededed', fg_color='#5c5b5b', command=Gauss_j_e)
+button5.pack(side='top', fill='x')
 
 menu.mainloop()
